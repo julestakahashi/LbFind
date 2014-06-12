ee77c61b4ad62f75f416aed008825e4aed0a4853
julestakahashi <julestakahashi@gmail.com>
2014-06-11 23:56:18 -0700

    load StofDoctrineExtensionsBundle and DoctrinFixturesBundle and delete vendor/monolog folder

--- src/DevTRW/LbFindBundle/Entity/Longboarder.php
+++ src/DevTRW/LbFindBundle/Entity/Longboarder.php
@@ -1,11 +1,11 @@
 <?php
 namespace DevTRW\LbFindBundle\Longboarder;
 
-use Doctrin\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
- * @ORM\Table(name=Longboarder)
+ * @ORM\Table(name="Longboarder")
  */
 class Longboarder
 {
@@ -35,4 +35,86 @@ class Longboarder
      * @ORM/Column(type="integer", nullable=true)
      */
     protected $age;
+
+
+    /**
+     * @return mixed
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * @param mixed $age
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDiscipline()
+    {
+        return $this->discipline;
+    }
+
+    /**
+     * @param mixed $discipline
+     */
+    public function setDiscipline($discipline)
+    {
+        $this->discipline = $discipline;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    /**
+     * @param mixed $location
+     */
+    public function setLocation($location)
+    {
+        $this->location = $location;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
 }
